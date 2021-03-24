b489324689866c74a7f3533780f1cc013f06f6a8
SMarkus27 <pereira.markus.27@hotmail.com>
2021-03-24 16:58:39 -0300

    fixed the expired time bug

--- Countdown/js/script.js
+++ Countdown/js/script.js
@@ -1,38 +1,53 @@
 const items = document.querySelectorAll('h4');
 
-const currentYear = new Date().getFullYear();
-const happyBirth = new Date(currentYear,4,27,18,0,0)
 
-const getTime = () =>{
+const getRemaindingTime = () =>{
+  let currentYear = new Date().getFullYear();
+
+  const month = 4;
+
+  if (new Date().getMonth() > month){
+
+    currentYear = currentYear + 1;
+    console.log(currentYear)
+
+  }
+  else{
+     currentYear = currentYear;
+  }
+  const futureTime = new Date(currentYear,month,27,18,0,0).getTime();
 
   const today = new Date().getTime();
-  const futureTime = new Date(happyBirth).getTime()
-
   const time = futureTime - today;
-  
+
   const oneDay = 60*60*24*1000;
   const oneHour = 60*60*1000;
   const oneMinute = 60*1000;
-  
-  let days = Math.floor(time / oneDay);
-  let hours = Math.floor((time%oneDay)/oneHour);
-  let minutes = Math.floor((time%oneHour)/oneMinute);
-  let seconds = Math.floor((time%oneMinute)/1000);
 
-  const values = [days,hours,minutes,seconds];
+  let days = Math.floor(time/oneDay);
+  let hours = Math.floor(time%oneDay/oneHour);
+  let minutes = Math.floor(time%oneHour/oneMinute);
+  let seconds = Math.floor(time%oneMinute/1000);
   
-  function format(item) {
-    if (item < 10) {
-      return (item = `0${item}`);
+  let values = [days,hours,minutes,seconds];
+  
+
+
+  function format (item){
+    if(item < 10){
+      return item = `0${item}`
     }
-    return item;
+    else{
+      return item
+    }
   }
 
-  items.forEach(function (item, index) {
-    item.innerHTML = format(values[index]);
-  });
+  items.forEach(function(item,index){
 
+    item.innerHTML = format(values[index]);
+
+  });
 }
 
-let countdown = setInterval(getTime,1000);
-getTime()
+const countdown = setInterval(getRemaindingTime,1000);
+getRemaindingTime()
